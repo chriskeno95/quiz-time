210f80910e97f3b3a6be95bca187f8cad6fad524
chris kennedy <chris-kennedy09@hotmail.com>
2023-01-23 17:34:02 +0000

    added the frame work for my questions

---
+++ assets/js/questions.js
@@ -0,0 +1,29 @@
+//in this file i was to present the questions that will be asked in the quiz. i will add them as objects within an array.
+
+let questions = [
+    {
+        "question": "question 1",
+        "choices": ["choice1","choice2","choice3"],
+        "answer" : "answer 1"
+    },
+    {
+        "question": "question 2",
+        "choices": ["choice1","choice2","choice3"],
+        "answer" : "answer 2"
+    },
+    {
+        "question": "question 3",
+        "choices": ["choice1","choice2","choice3"],
+        "answer" : "answer 3"
+    },
+    {
+        "question": "question 4",
+        "choices": ["choice1","choice2","choice3"],
+        "answer" : "answer 4"
+    },
+    {
+        "question": "question 5",
+        "choices": ["choice1","choice2","choice3"],
+        "answer" : "answer 5"
+    },
+]
