45314c471336d9d55008b4494c3f6b6878e25a3c
chris kennedy <chris-kennedy09@hotmail.com>
2023-01-23 17:38:06 +0000

    added comments to question file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,29 +1,29 @@
-//in this file i was to present the questions that will be asked in the quiz. i will add them as objects within an array.
+//this file will contain the questions that will be asked in the quiz. i will add them as objects within an array.
 
 let questions = [
-    {
+    { // array index 0 = question 1
         "question": "question 1",
         "choices": ["choice1","choice2","choice3"],
         "answer" : "answer 1"
     },
-    {
+    { // array index 1 = question 2
         "question": "question 2",
         "choices": ["choice1","choice2","choice3"],
         "answer" : "answer 2"
     },
-    {
+    {// array index 2 = question 3
         "question": "question 3",
         "choices": ["choice1","choice2","choice3"],
         "answer" : "answer 3"
     },
-    {
+    {// array index 3 = question 4
         "question": "question 4",
         "choices": ["choice1","choice2","choice3"],
         "answer" : "answer 4"
     },
-    {
+    {// array index 4 = question 5
         "question": "question 5",
-        "choices": ["choice1","choice2","choice3"],
+        "choices": ["choice1","choice2", "choice3"],
         "answer" : "answer 5"
     },
 ]
